05def4df12fe0bf69e9c0a86767a730667470b6c
Seakee <seakee23@163.com>
2017-05-15 09:36:59 +0800

    new routes

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,28 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/admin', 'AdminController@index')->name('admin');
+
+
+Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'checkPermission']], function() {
+    
+    Route::get('/', 'AdminController@index')->name('admin');
+    
+    Route::group(['prefix' => 'menu', 'namespace' => 'menu'], function (){
+        Route::get('/admin', 'MenuController@admin')->name('menu.admin');
+        Route::get('/home', 'MenuController@home')->name('menu.home');
+        Route::get('/createAdminMenu', 'MenuController@createAdminMenu')->name('menu.admin.create');
+        Route::get('/editAdminMenu', 'MenuController@editAdminMenu')->name('menu.admin.edit');
+        Route::get('/editHomeMenu', 'MenuController@editHomeMenu')->name('menu.home.edit');
+        Route::get('/createHomeMenu', 'MenuController@createHomeMenu')->name('menu.home.create');
+        Route::get('/delete', 'MenuController@deleteMenu')->name('menu.delete');
+        Route::get('/changeDisplay', 'MenuController@changeDisplay')->name('menu.changeDisplay');
+        Route::post('/create', 'MenuController@createMenu')->name('menu.create');
+        Route::post('/edit', 'MenuController@editMenu')->name('menu.edit');
+    });
+
+    Route::group(['prefix' => 'user', 'namespace' => 'user'], function (){
+        Route::get('/user/index', 'UserController@index')->name('user.index');
+        Route::get('/role/index', 'RoleController@index')->name('role.index');
+        Route::get('/permission/index', 'PermissionController@index')->name('permission.index');
+    });
+});
