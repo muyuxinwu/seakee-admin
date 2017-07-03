c5b7769c449174b819e8c8f92b47be2f5ad0ab46
Seakee <seakee23@163.com>
2017-07-03 17:18:05 +0800

    menu icon

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,8 +2,8 @@
     <li class="header">HEADER</li>
     <!-- Optionally, you can add icons to the links -->
     @foreach($sidebarMenu as $key => $menu)
-        <li class="treeview">
-            <a href="{{ $menu['menu_url'] }}"><i class="fa fa-circle-o"></i> <span>{{ $menu['menu_name'] }}</span>
+        <li class="treeview" id="{{ $menu['id'] }}">
+            <a href="{{ $menu['menu_url'] }}"><i class="fa {{ $menu['icon'] }}"></i> <span>{{ $menu['menu_name'] }}</span>
                 @if(isset($menu['nodes']))
                     <span class="pull-right-container">
                         <i class="fa fa-angle-left pull-right"></i>
@@ -13,8 +13,8 @@
             @if(isset($menu['nodes']))
                 <ul class="treeview-menu">
                     @foreach($menu['nodes'] as $key => $menu)
-                        <li>
-                            <a href="{{ $menu['menu_url'] }}"><i class="fa fa-circle-o"></i>
+                        <li class="" id="{{ $menu['id'] }}">
+                            <a href="{{ $menu['menu_url'] }}"><i class="fa {{ $menu['icon'] }}"></i>
                                 <span>{{ $menu['menu_name'] }}</span>
                                 @if(isset($menu['nodes']))
                                     <span class="pull-right-container">
@@ -25,8 +25,12 @@
                             @if(isset($menu['nodes']))
                                 <ul class="treeview-menu">
                                     @foreach($menu['nodes'] as $key => $menu)
-                                        <li><a href="{{ $menu['menu_url'] }}"><i class="fa fa-circle-o"></i>
-                                                <span>{{ $menu['menu_name'] }}</span></a></li>
+                                        <li class="" id="{{ $menu['id'] }}">
+                                            <a href="{{ $menu['menu_url'] }}">
+                                                <i class="fa {{ $menu['icon'] }}"></i>
+                                                <span>{{ $menu['menu_name'] }}</span>
+                                            </a>
+                                        </li>
                                     @endforeach
                                 </ul>
                             @endif
