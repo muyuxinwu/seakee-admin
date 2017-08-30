240eebf7c5a0d73095d95c802891b19f8e021937
Seakee <seakee23@163.com>
2017-08-30 22:32:47 +0800

    修正RoleTableSeeder

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -12,24 +12,24 @@ class RoleTableSeeder extends Seeder
     public function run()
     {
         DB::table('roles')->insert([
-            'name' => '超级管理员',
-            'display_name' => 'Super_Admin',
+            'name' => 'Super_Admin',
+            'display_name' => '超级管理员',
             'description' => '超级管理员，拥有所有权限',
             'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
             'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
         ]);
 
         DB::table('roles')->insert([
-            'name' => '管理员',
-            'display_name' => 'Admin',
+            'name' => 'Admin',
+            'display_name' => '管理员',
             'description' => '按权限分配的管理员',
             'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
             'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
         ]);
 
         DB::table('roles')->insert([
-            'name' => '会员',
-            'display_name' => 'Member',
+            'name' => 'Member',
+            'display_name' => '会员',
             'description' => '普通会员，只用拥有前台权限',
             'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
             'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
