5d4c24af6ecb81aa3906d845e1e1f3b40c61a1eb
Seakee <seakee23@163.com>
2017-08-30 09:59:44 +0800

    权限管理路由列表显示优化

--- resources/views/user/permissionIndex.blade.php
+++ resources/views/user/permissionIndex.blade.php
@@ -138,7 +138,7 @@
     </div>
     <!-- route list Modal -->
     <div class="modal fade" id="routeList" tabindex="1" role="dialog" aria-labelledby="routeList">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog" role="document" style="width:800px;">
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
