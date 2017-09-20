5e8dac4934322bdba34cd1929db2df8bf195e655
Seakee <seakee23@163.com>
2017-09-20 18:20:06 +0800

    RequestParams服务

---
+++ app/Services/RequestParamsService.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * File: RequestParamsService.php
+ * Author: Seakee <seakee23@163.com>
+ * Date: 2017/9/20 17:50
+ * Description:
+ */
+
+namespace App\Services;
+
+
+class RequestParamsService
+{
+	/**
+	 * 获取指定key的输入值
+	 *
+	 * @param $paramKeys
+	 * @param $request
+	 *
+	 * @return array
+	 */
+	public function params($paramKeys, $request)
+	{
+		foreach ($paramKeys as $key) {
+			if ($request->has($key)) {
+				$data[$key] = $request->input($key);
+			}
+		}
+
+		return $data ?? [];
+	}
+}
