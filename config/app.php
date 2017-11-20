857fd0c0ccd9c130d463d4e100c264d3342ffc03
Seakee <seakee23@163.com>
2017-11-20 21:00:56 +0800

    加入七牛存储

--- config/app.php
+++ config/app.php
@@ -170,6 +170,7 @@ return [
         /*
          * Application Service Providers...
          */
+        zgldh\QiniuStorage\QiniuFilesystemServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
